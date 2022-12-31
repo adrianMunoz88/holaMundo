765d493bbad2a87b9fa6aa3eae8ac2dff69dd6b6
dmunozs <munoz_adrian@hotmail.com>
2022-12-30 18:02:32 -0600

    Se quito la linea de codigo inservible

--- src/com/acme/PersonaRegistro.java
+++ src/com/acme/PersonaRegistro.java
@@ -4,8 +4,6 @@ public record PersonaRegistro(String nombre, String apellido) {
 
     @Override
     public String toString() {
-        //No pongas basura Carnal
-        //System.out.println("Esta instruccion no sirve");
         return "%s %s".formatted(nombre,apellido);
     }
 }
